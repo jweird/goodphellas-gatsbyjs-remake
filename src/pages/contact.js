76aff980cb2d6b6e50b9e97a1ac832dff8129876
unknown <juanmiindset@gmail.com>
2020-03-14 15:58:14 -0700

    Changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,18 +9,25 @@ const ContactPage = () => (
   <Layout>
     <img src={MainImg} width="35%" style={{ paddingTop: "5vh" }}></img>
     <div className={LayoutStyles.formcont}>
-      <Form name="contact" method="POST" data-netlify="true">
+      <Form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group as={Col} controlId="formGridName">
           <Form.Label>Artist Name</Form.Label>
-          <Form.Control type="email" placeholder="Artist Name" />
+          <Form.Control type="text" placeholder="Artist Name" />
         </Form.Group>
         <Form.Group as={Col} controlId="formGridLink">
           <Form.Label>Private Soundcloud Link</Form.Label>
-          <Form.Control type="email" placeholder="Private Soundcloud Link" />
+          <Form.Control type="url" placeholder="Private Soundcloud Link" />
         </Form.Group>
         <div className={LayoutStyles.buttcent}>
           <Button className={LayoutStyles.formbutt} type="submit">
